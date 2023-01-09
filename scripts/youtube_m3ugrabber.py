8900039a36960955142cffa232023506ff65ca55
pk3011 <118091725+pk3011@users.noreply.github.com>
2023-01-09 17:21:30 +0530

    Update youtube_m3ugrabber.py

--- scripts/youtube_m3ugrabber.py
+++ scripts/youtube_m3ugrabber.py
@@ -34,7 +34,7 @@ def grab(url):
             tuner += 5
     print(f"{link[start : end]}")
 
-print('#EXTM3U x-tvg-url="https://github.com/botallen/epg/releases/download/latest/epg.xml"')
+print('#EXTM3U x-tvg-url="http://indtv.me/guide.gz"')
 #s = requests.Session()
 with open('../youtube_channel_info.txt') as f:
     for line in f:
